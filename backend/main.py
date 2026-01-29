8df63940b811b70a0e843e841bc35881747a973d
StephenHerrera <stephendherrera05@gmail.com>
2026-01-29 13:17:25 -0500

    Add SQLite database setup and models

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from database import engine, Base
+from models import TransactionDB
 
 # Main server object that will handle incoming requests.
 app = FastAPI()
+
+#creating corresponding tables in database that dont exist
+Base.metadata.create_all(bind=engine)
+
+# Creating data model to only accept valid input from the user
+class Transaction(BaseModel):
+    amount: float
+    description: str
+    category: str
 
 # Temporary storage for transactions.
 # Python list to focus on learning APIs.
@@ -19,6 +31,6 @@ def get_transactions():
 
 # Add a new transaction.
 @app.post("/transactions")
-def add_transaction(transaction: dict):
+def add_transaction(transaction: Transaction):
     transactions.append(transaction)
     return {"message": "Transaction added"}
---
+++ backend/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, Float, String
+from database import Base
+
+class TransactionDB(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float, nullable=False)
+    description = Column(String, nullable=False)
+    category = Column(String, nullable=False)
