26fe7720485e51345c5d10bc765ab33129daf077
StephenHerrera <stephendherrera05@gmail.com>
2026-02-04 14:24:49 -0500

    Add initial SwiftUI frontend project structure and Xcode gitignore rules

---
+++ frontend/CashflowCopilot/CashflowCopilot/CashflowCopilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashflowCopilotApp.swift
+//  CashflowCopilot
+//
+//  Created by Stephen Herrera on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct CashflowCopilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
